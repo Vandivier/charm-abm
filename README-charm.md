37ff3414234648d62a7625e3797819200481219e
Vandivier <vandivier_john@yahoo.com>
2017-09-16 16:00:16 -0400

    build asx readme and rename README-charm

---
+++ README-charm.md
@@ -0,0 +1,7 @@
+# charm-abm
+Third attempt to build an agent based model of education.
+
+To say I was influenced by [asx](https://github.com/backspaces/asx) would be an understatement.
+
+`npm install` depends on node-gyp via dat.GUI. node-gyp requires minor installation including python 2.x:
+https://github.com/nodejs/node-gyp
--- js/build.js
+++ js/build.js
@@ -5,6 +5,7 @@
 'use strict'
 
 const BUILD     = require('./build-utils');
+const butternut = require('butternut');
 const { exec }  = require('child_process');
 const shell     = require('shelljs');
 
@@ -23,10 +24,9 @@ shell.exec('node ./bin/wraplibs.js');
 shell.exec('sh ./bin/buildscripts.sh models/src');      // must use bash. on Windows we can use Git Bash
 
 //  npm run build-dist
-//shell.exec('rollup -c');
-//shell.cp('src/*', 'dist/AS');
-
-//  squash dist/AS.js > dist/AS.min.js
-//  squash dist/AS.module.js > dist/AS.module.min.js
-//  npm run build-docs 
-//  cp docs/README.md .
+shell.exec('rollup -c');
+shell.cp('src/*', 'dist/AS');
+shell.exec('squash dist/AS.js > dist/AS.min.js');
+shell.exec('squash dist/AS.module.js > dist/AS.module.min.js');
+//  npm run build-docs "cp -Rp `bin/pkgkey.js docsfiles` docs"
+shell.cp('docs/README.md', '.');
