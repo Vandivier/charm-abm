00ab8ade48672e1d42ed093c19bf00074c8cefc7
Vandivier <vandivier_john@yahoo.com>
2017-10-01 19:32:04 -0400

    move more AS logic into the AS package

--- src/app.js
+++ src/app.js
@@ -1,55 +1,9 @@
-// TODO: webpack loader so I can use await. This might be the same prob the AS supermodule had.
-
 'use strict'
 
-// webpack-style async module loading
-
-const pStats = import(
-                  /* webpackChunkName: "stats" */
-                  /* webpackMode: "eager" */
-                  'stats.js'
-                );
-const pThree = import(
-                  /* webpackChunkName: "three" */
-                  /* webpackMode: "eager" */
-                  'three'
-                );
-
-Promise.all([pStats, pThree]).then(function(arrModules){
-    window.Stats = arrModules[0];
-    window.THREE = arrModules[1] || {};
-    /*
-    debugger
-    window.THREE = arrModules[1] && arrModules[1].default || {};
-    */
-});
-
-// pOrbitControls needs THREE
-const pOrbitControls = import(
-                  /* webpackChunkName: "OrbitControls" */
-                  /* webpackMode: "lazy" */
-                  './lib/OrbitControls.wrapper.js'
-                );
-
-// TODO: requires SCSS loader. As an HTML script tag for now.
-const pDat = Promise.resolve();
-
-Promise.all([pDat, pOrbitControls]).then(function(arrModules){
-    //var dat = arrModules[0];
-    var OrbitControls = arrModules[1];
-
-    const pAs = import(
-                  /* webpackChunkName: "asx-abm" */
-                  /* webpackMode: "lazy" */
-                  'asx-abm'
-                );
-
-    pAs.then(_as => {
-        window.AS = _as;
-        initModel();
-    });
-});
+const AS = require('asx-abm');
 
 function initModel() {
     AS.util.setScript('static/scripts/diffuse.js');
 }
+
+initModel();
