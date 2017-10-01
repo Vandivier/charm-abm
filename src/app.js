b2eb79587d7601752e76bdcc137382d84a276ae2
Vandivier <vandivier_john@yahoo.com>
2017-10-01 14:40:32 -0400

    put Stats on window

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const pThree = import(
                 );
 
 Promise.all([pStats, pThree]).then(function(arrModules){
-    var Stats = arrModules[0];
+    window.Stats = arrModules[0];
     window.THREE = arrModules[1] || {};
     /*
     debugger
@@ -31,7 +31,7 @@ const pOrbitControls = import(
                   './lib/OrbitControls.wrapper.js'
                 );
 
-// TODO: requires SCSS loader. As an HTML script for now.
+// TODO: requires SCSS loader. As an HTML script tag for now.
 const pDat = Promise.resolve();
 
 Promise.all([pDat, pOrbitControls]).then(function(arrModules){
@@ -52,32 +52,4 @@ Promise.all([pDat, pOrbitControls]).then(function(arrModules){
 
 function initModel() {
     AS.util.setScript('static/scripts/diffuse.js');
-    /*
-    let app = document.location.search.substring(1) || 'static/scripts/diffuse'
-    if (app.endsWith('/')) app = app.slice(0, -1)       // trailing / problem with SimpleHTTPServer. ref: https://github.com/backspaces/asx/issues/11
-    let [appDir, appName] = app.split('/')
-    if (appName === undefined) {
-        console.log('Note: app path is "dir/name", dir is "scripts" or "src"')
-        console.log('..Using "scripts" as default')
-        appName = appDir
-        appDir = 'scripts'
-    }
-
-    const loc = `./${appDir}/${appName}.js`
-    console.log('running:', loc, 'dir:', appDir, 'name:', appName + '.js')
-    document.title = `asx:${appName}`
-
-    switch (appDir) {
-        case 'scripts':
-            AS.util.setScript(loc);
-            break
-        case 'src':
-            AS.util.setScript(loc, {
-                type: 'module'
-            });
-            break
-        default:
-            throw `Oops: ${appDir} not valid dir`
-    }
-    */
 }
