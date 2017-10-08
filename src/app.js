0cff7c9a26855af16acdc5c502c769a665e37959
Vandivier <vandivier_john@yahoo.com>
2017-10-08 00:23:11 -0400

    disturb turtle location on enter

--- src/app.js
+++ src/app.js
@@ -14,18 +14,26 @@ function initModel() {
 
     const model = new DiffuseModel(document.body, options)
     window.model = model; //facilitate debugging
+    window.aturtle = model.turtles[0]; //facilitate debugging
+    window.apatch = model.patches[0]; //facilitate debugging
 
     model.setup()
     model.start()
 
     //  TODO: freeze model time without preventing model rotation, zoom, etc?
     window.onkeyup = function (e) {
-        if(e.keyCode == 32){
+        if (e.keyCode == 32) { // space bar
             if (model.anim.stopped) {
                 model.anim.start()
             } else {
                 model.anim.stop()
             }
+        }
+
+        if (e.keyCode == 13) { // enter key
+            model.turtles.ask(turtle => { // shake up turtle locations
+                turtle.moveTo(AS.util.oneOf(model.patches))
+            });
         }
     }
 } 
