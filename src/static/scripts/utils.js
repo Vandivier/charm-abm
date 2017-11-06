525a1da68018f92284efadb8dbc59e9eb5ae7da8
John Vandivier <john.vandivier@halfaker.com>
2017-11-06 10:21:04 -0500

    add unique util

--- src/static/scripts/utils.js
+++ src/static/scripts/utils.js
@@ -10,4 +10,16 @@ utils.rev = function(arr, f) {
     }
 }
 
+// ref: https://stackoverflow.com/questions/11246758/how-to-get-unique-values-in-an-array
+// ref: https://jsperf.com/get-unique-elements
+Array.prototype.unique = function() {
+    var arr = [];
+    for(var i = 0; i < this.length; i++) {
+        if(!arr.includes(this[i])) {
+            arr.push(this[i]);
+        }
+    }
+    return arr; 
+}
+
 module.exports = utils;
