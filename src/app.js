1ff611c94e79adf51abd7cc0ec273a1056ef7fcb
John Vandivier <john.vandivier@halfaker.com>
2017-12-02 11:33:05 -0500

    fCustomRendererOptions

--- src/app.js
+++ src/app.js
@@ -10,11 +10,52 @@ function main() {
 }
 
 function initModel() {
-    const options = AS.Model.defaultWorld(2, 50)
-    options.minX = 2 * options.minX
-    options.maxX = 2 * options.maxY
+    const optionsWorld = AS.Model.defaultWorld(2, 50)
+    optionsWorld.minX = 2 * optionsWorld.minX
+    optionsWorld.maxX = 2 * optionsWorld.maxY
 
-    const model = new ActiveModel(document.body, options)
+    // ref: AS.premodule.js, class Three, static defaultOption
+    static fCustomRendererOptions(useThreeHelpers = true, useUIHelpers = true) {
+        const options = {
+            // include me in options so Model can instanciate me!
+            Renderer: Three, // REMIND: use string.
+            orthoView: false, // 'Perspective', 'Orthographic'
+            clearColor: 0x000000, // clear to black
+            useAxes: useThreeHelpers, // show x,y,z axes
+            useGrid: useThreeHelpers, // show x,y plane
+            useControls: useThreeHelpers, // navigation. REMIND: control name?
+            useStats: useUIHelpers, // show fps widget
+            useGUI: useUIHelpers, // activate dat.gui UI
+            // meshes: {
+            patches: {
+                meshClass: 'PatchesMesh'
+            },
+            turtles: {
+                meshClass: 'QuadSpritesMesh'
+                // meshClass: 'PointsMesh'
+            },
+            links: {
+                meshClass: 'LinksMesh'
+            }
+            // }
+        };
+        // util.forEach(options.meshes, (val, key) => {
+        //   const Mesh = Meshes[val.meshClass]
+        //   const meshOptions = Mesh.options()
+        //   val.options = meshOptions
+        // })
+        util.forEach(options, (val, key) => {
+            if (val.meshClass) {
+                const Mesh = Meshes[val.meshClass];
+                const meshOptions = Mesh.options();
+                val.options = meshOptions;
+            }
+        });
+
+        return options
+    }
+
+    const model = new ActiveModel(document.body, optionsWorld)
     window.model = model; //facilitate debugging
     window.aturtle = model.turtles[0]; //facilitate debugging
     window.apatch = model.patches[0]; //facilitate debugging
